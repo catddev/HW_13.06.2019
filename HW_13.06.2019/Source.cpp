f0a779378847ca1822861817b67777c1b333fea2
айкеновад <@205_8>
2019-06-14 13:28:59 +0600

    -

--- HW_13.06.2019/Source.cpp
+++ HW_13.06.2019/Source.cpp
@@ -1,7 +1,10 @@
 ﻿#include<iostream>
 #include<string>
+#include<fstream>
 #include<algorithm>
 #include<deque>
+#include<set>
+#include<Windows.h>
 using namespace std;
 
 //Напишите программу, в которую вводится имя текстового файла.
@@ -10,8 +13,30 @@ using namespace std;
 
 int main() {
 
+	ifstream in_file("in.txt");
+	string str;
+	deque<string> deq;
 
+	while (!in_file.eof()) {
+		in_file >> str;
+		deq.push_back(str);//запись в декью
+	}
 
+	for_each(deq.begin(), deq.end(), [](string s) {
+		transform(s.begin(), s.end(), s.begin(), tolower);//перевод в нижний регистр
+	});
+
+	sort(deq.begin(), deq.end());//сортировка
+
+	//удаление дубликатов
+	deq.erase(
+		unique(deq.begin(), deq.end()),//unique() is a condition to erase
+		deq.end());
+
+	//результат
+	for_each(deq.begin(), deq.end(), [](string s) {
+		cout << s << endl;
+	});
 
 
 	system("pause");
